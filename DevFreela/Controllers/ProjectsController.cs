a11dadfd764686e4dd05489a5ef654c359f01e37
Gabriel Silva de Oliveira <gso@cin.ufpe.br>
2023-08-30 16:12:40 -0300

    Estudando Actions e Controllers

---
+++ DevFreela/Controllers/ProjectsController.cs
@@ -0,0 +1,64 @@
+﻿using DevFreela.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(GetById),new {id = createProject.Id}, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id,[FromBody] UpdateProjectModel createProject)
+        {
+            if (createProject.Description.Length > 50)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id )
+        {
+            
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id,[FromBody] CreateCommentModel createComment)
+        {
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+           
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Controllers
+{
+
+
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([Frombody] CreateUserModel createUserModel)
+    }
+}
